a712658e8b27e3a1b114c0af51fc1a79a781d96c
LuanFMelo <luan.farias@stone.com.br>
2024-06-14 02:07:22 -0300

    feat/main-file

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/GET")
+async def get():
+    return {"message": "GET something"}
+
+@app.post("/POST")
+async def post():
+    return {"message": "GET something"}
+
+@app.put("/PUT")
+async def put():
+    return {"message": "PUT something"}
+
+@app.delete("/DELETE")
+async def delete():
+    return {"message": "DELETE something"}
